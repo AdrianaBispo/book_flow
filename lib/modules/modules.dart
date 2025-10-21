0c255747040ea3ca8b7df4380ee8f6e2f6e0f6b4
adrianaBispo283 <adriana.bispo283@gmail.com>
2025-10-21 20:22:26 -0300

    chore: adcionado home/home.dart

--- lib/modules/modules.dart
+++ lib/modules/modules.dart
@@ -5,8 +5,5 @@ export 'package:supabase_flutter/supabase_flutter.dart';
 export 'package:flutter_screenutil/flutter_screenutil.dart';
 export 'package:go_router/go_router.dart';
 
-//views
-export 'modules.dart';
-
-//BLOC
 export 'login/login.dart';
+export 'home/home.dart';
