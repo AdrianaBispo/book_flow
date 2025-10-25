f5f04f82b422bb9e253fb0ad493f8adf3f9fb26b
adrianaBispo283 <adriana.bispo283@gmail.com>
2025-10-24 21:30:00 -0300

    feat: adcionado chamada de BibliotecaBookCard

--- lib/modules/biblioteca/presenter/view/biblioteca_view.dart
+++ lib/modules/biblioteca/presenter/view/biblioteca_view.dart
@@ -1,4 +1,34 @@
 import 'package:flutter/material.dart';
+import '../widgets/biblioteca_book_card.dart';
+
+final List<Map<String, String>> mockFavoriteBooks = [
+  {
+    'title': 'O Estranho Caso do Dr. Jekyll e Mr. Hyde',
+    'author': 'Robert Louis Stevenson',
+    'genre': 'Gótico',
+  },
+  {'title': '1984', 'author': 'George Orwell', 'genre': 'Distopia'},
+  {
+    'title': 'Cem Anos de Solidão',
+    'author': 'Gabriel García Márquez',
+    'genre': 'Realismo Mágico',
+  },
+  {
+    'title': 'A Revolução dos Bichos',
+    'author': 'George Orwell',
+    'genre': 'Sátira Política',
+  },
+  {
+    'title': 'O Pequeno Príncipe',
+    'author': 'Antoine de Saint-Exupéry',
+    'genre': 'Fábula',
+  },
+  {
+    'title': 'Orgulho e Preconceito',
+    'author': 'Jane Austen',
+    'genre': 'Romance',
+  },
+];
 
 class BibliotecaView extends StatefulWidget {
   const BibliotecaView({super.key});
@@ -10,6 +40,24 @@ class BibliotecaView extends StatefulWidget {
 class _BibliotecaViewState extends State<BibliotecaView> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Minha Bilioteca')),
+      body: ListView.separated(
+        padding: const EdgeInsets.all(16.0),
+        itemCount: mockFavoriteBooks.length,
+        itemBuilder: (context, index) {
+          final book = mockFavoriteBooks[index];
+          return BibliotecaBookCard(
+            title: book['title']!,
+            author: book['author']!,
+            genre: book['genre']!,
+            imageUrl:
+                'https://www.gutenberg.org/cache/epub/18452/pg18452.cover.medium.jpg', // Usado para o placeholder de cor
+          );
+        },
+        separatorBuilder: (context, index) =>
+            Divider(color: Theme.of(context).dividerColor, height: 24),
+      ),
+    );
   }
 }
