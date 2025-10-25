37be619af16f23f5bd1161d7f08fa015a1695fbf
adrianaBispo283 <adriana.bispo283@gmail.com>
2025-10-24 21:19:47 -0300

    feat: criado BibliotecaView

---
+++ lib/modules/biblioteca/presenter/view/biblioteca_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BibliotecaView extends StatefulWidget {
+  const BibliotecaView({super.key});
+
+  @override
+  State<BibliotecaView> createState() => _BibliotecaViewState();
+}
+
+class _BibliotecaViewState extends State<BibliotecaView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
