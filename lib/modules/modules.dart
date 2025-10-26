a5123b430a665e854a91819231df1c9ce04b1f24
adrianaBispo283 <adriana.bispo283@gmail.com>
2025-10-26 20:41:01 -0300

    feat: adcionado flutter_localizations

--- lib/modules/modules.dart
+++ lib/modules/modules.dart
@@ -4,6 +4,8 @@ export  'package:provider/provider.dart';
 export 'package:supabase_flutter/supabase_flutter.dart';
 export 'package:flutter_screenutil/flutter_screenutil.dart';
 export 'package:go_router/go_router.dart';
+export 'package:flutter_localizations/flutter_localizations.dart';
+
 
 export 'login/login.dart';
 export 'biblioteca/biblioteca.dart';
