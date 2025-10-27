c1fb628235b50d4f82776495d8d4464d72101bf4
adrianaBispo283 <adriana.bispo283@gmail.com>
2025-10-27 20:11:58 -0300

    fixed: adcionado author

--- lib/modules/search/domain/entities/result_search_entity.dart
+++ lib/modules/search/domain/entities/result_search_entity.dart
@@ -4,5 +4,5 @@ abstract class ResultSearchEntity {
     final String author;
     final String? cover_url;
     final String? download_url;
-    ResultSearchEntity({required this.id, required this.title, this.cover_url, this.download_url});
+    ResultSearchEntity({required this.id, required this.author, required this.title, this.cover_url, this.download_url});
 }
