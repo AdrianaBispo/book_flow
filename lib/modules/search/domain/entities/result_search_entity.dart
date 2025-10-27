380accfe228416079e396c3d3a8e127d4f37a18a
AdrianaBispo <86270127+AdrianaBispo@users.noreply.github.com>
2025-10-27 15:52:20 -0300

    refactor: renomeado para ResultSearchEntity

--- lib/modules/search/domain/entities/result_search_entity.dart
+++ lib/modules/search/domain/entities/result_search_entity.dart
@@ -1,8 +1,8 @@
-abstract class ResultSearch {
+abstract class ResultSearchEntity {
     final int id;
     final String title;
     final String author;
     final String? cover_url;
     final String? download_url;
-    ResultSearch({required this.id, required this.title, this.cover_url, this.download_url});
+    ResultSearchEntity({required this.id, required this.title, this.cover_url, this.download_url});
 }
