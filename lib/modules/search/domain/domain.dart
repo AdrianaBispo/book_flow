42168e2e08d6ba403642b32ac6b8710cd6090c73
adrianaBispo283 <adriana.bispo283@gmail.com>
2025-10-27 20:12:35 -0300

    fixed: repositories

--- lib/modules/search/domain/domain.dart
+++ lib/modules/search/domain/domain.dart
@@ -1,4 +1,4 @@
 export 'entities/entities.dart';
 export 'usecases/usecases.dart';
-export 'exceptions/exceptions_login.dart';
-export 'repositories/repostiories.dart';
+export 'exceptions/exceptions_search.dart';
+export 'repositories/repositories.dart';
