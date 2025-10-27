7301ebb37b47fbfaade875af4df40bcb61518793
adrianaBispo283 <adriana.bispo283@gmail.com>
2025-10-26 22:03:42 -0300

    removing the import directive

--- lib/modules/login/domain/repositories/login_repository.dart
+++ lib/modules/login/domain/repositories/login_repository.dart
@@ -1,6 +1,5 @@
 import 'package:myapp/modules/modules.dart';
 import 'package:myapp/utils/utils.dart';
-import '../domain.dart';
 
 abstract class LoginRepository {
   Future<Either<AppException, UserLoginEntity>> loginWithEmailAndPassword(
