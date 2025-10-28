0f13519d5785617676eb584acf387b6ab6902226
adrianaBispo283 <adriana.bispo283@gmail.com>
2025-10-28 20:47:33 -0300

    refactor: alterado o nome das classes

--- lib/modules/search/domain/exceptions/exceptions_search.dart
+++ lib/modules/search/domain/exceptions/exceptions_search.dart
@@ -4,14 +4,14 @@ class ServerCredentialsException extends AppException {
   ServerCredentialsException({required super.message, super.stackTrace});
 }
 
-class NetworkException extends AppException {
-  NetworkException({required super.message, super.stackTrace});
+class NetworkSearchException extends AppException {
+  NetworkSearchException({required super.message, super.stackTrace});
 }
 
 class UnknownSearchException extends AppException {
   UnknownSearchException({required super.message, super.stackTrace});
 }
 
-class DatabaseException extends AppException {
-  DatabaseException({required super.message, super.stackTrace});
+class DatabaseSearchException extends AppException {
+  DatabaseSearchException({required super.message, super.stackTrace});
 }
