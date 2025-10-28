56a299ef3d1288373b6b65df167b017abff5c871
adrianaBispo283 <adriana.bispo283@gmail.com>
2025-10-28 20:44:22 -0300

    refactor: trocada a tabela

--- lib/modules/search/data/datasources/remote/search_datasource_remote_impl.dart
+++ lib/modules/search/data/datasources/remote/search_datasource_remote_impl.dart
@@ -11,7 +11,7 @@ class SearchDataSourceImpl implements SearchDatasourceRemote {
       // Se não houver texto de busca, traz todos os livros
       if (searchText.trim().isEmpty) {
         final response = await supabase
-            .from('books')
+            .from('vw_ebook')
             .select('*')
             .order('title', ascending: true);
 
@@ -21,17 +21,15 @@ class SearchDataSourceImpl implements SearchDatasourceRemote {
 
       // Caso haja texto de busca, filtra por título OU autor
       final response = await supabase
-          .from('books')
+          .from('vw_ebook')
           .select('*')
           .or('title.ilike.%$searchText%,author.ilike.%$searchText%')
           .order('title', ascending: true);
 
       final List data = response;
       return data.map((item) => ResultSearchDto.fromJson(item)).toList();
-    } on PostgrestException catch (e) {
-      throw DatabaseException(message: 'Erro ao buscar dados: ${e.message}');
     } catch (e) {
-      throw UnknownSearchException(message: 'Erro inesperado: $e');
+      rethrow;
     }
   }
 }
