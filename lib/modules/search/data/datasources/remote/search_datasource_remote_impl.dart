6443565e64110f9a6aae0987f7e77f89ab1eb4b0
adriana.santos.sudoeste <adriana.santos.sudoeste@gmail.com>
2025-10-28 18:03:59 +0000

    refactor: alterado nome da class

--- lib/modules/search/data/datasources/remote/search_datasource_remote_impl.dart
+++ lib/modules/search/data/datasources/remote/search_datasource_remote_impl.dart
@@ -1,17 +1,22 @@
 import 'package:myapp/modules/modules.dart';
 
-class SearchDataSourceImpl implements SearchDatasourceRemote {
-  final SupabaseClient supabase;
+class SearchDataSourceRemoteImpl implements SearchDatasourceRemote {
+  final SupabaseClient supabaseClient;
 
-  SearchDataSourceImpl(this.supabase);
+  SearchDataSourceRemoteImpl(this.supabaseClient);
 
   @override
   Future<List<ResultSearchDto>> search(String searchText) async {
     try {
       // Se não houver texto de busca, traz todos os livros
       if (searchText.trim().isEmpty) {
+<<<<<<< HEAD
         final response = await supabase
             .from('vw_ebook')
+=======
+        final response = await supabaseClient
+            .from('books')
+>>>>>>> bfbc6da (refactor: alterado nome da class)
             .select('*')
             .order('title', ascending: true);
 
@@ -20,8 +25,13 @@ class SearchDataSourceImpl implements SearchDatasourceRemote {
       }
 
       // Caso haja texto de busca, filtra por título OU autor
+<<<<<<< HEAD
       final response = await supabase
           .from('vw_ebook')
+=======
+      final response = await supabaseClient
+          .from('books')
+>>>>>>> bfbc6da (refactor: alterado nome da class)
           .select('*')
           .or('title.ilike.%$searchText%,author.ilike.%$searchText%')
           .order('title', ascending: true);
