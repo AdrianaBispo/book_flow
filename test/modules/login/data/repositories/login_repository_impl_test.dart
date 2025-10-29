2c75c1a3dd1bf1e102b8d726c8fb56e1e8fd5f09
adrianaBispo283 <adriana.bispo283@gmail.com>
2025-10-29 20:19:30 -0300

    #

--- test/modules/login/data/repositories/login_repository_impl_test.dart
+++ test/modules/login/data/repositories/login_repository_impl_test.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mocktail/mocktail.dart';
+import 'package:mockito/mockito.dart';
 import 'package:myapp/modules/modules.dart';
 
 class MockLoginRepositoryImpl extends Mock implements LoginRepositoryImpl {}
