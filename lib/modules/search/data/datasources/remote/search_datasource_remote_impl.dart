ad26a95cee18a79e452f80baf8457b510b695c94
adriana.santos.sudoeste <adriana.santos.sudoeste@gmail.com>
2025-10-29 17:37:16 +0000

    conflito resolvido

--- lib/modules/search/data/datasources/remote/search_datasource_remote_impl.dart
+++ lib/modules/search/data/datasources/remote/search_datasource_remote_impl.dart
@@ -1,48 +1,30 @@
 import 'package:myapp/modules/modules.dart';
 
 class SearchDataSourceRemoteImpl implements SearchDatasourceRemote {
-  final SupabaseClient supabaseClient;
+  final SupabaseClient supabase;
 
-  SearchDataSourceRemoteImpl(this.supabaseClient);
+  SearchDataSourceRemoteImpl(this.supabase);
 
   @override
   Future<List<ResultSearchDto>> search(String searchText) async {
     try {
       // Se não houver texto de busca, traz todos os livros
       if (searchText.trim().isEmpty) {
-<<<<<<< HEAD
         final response = await supabase
             .from('vw_ebook')
-=======
-        final response = await supabaseClient
-<<<<<<< HEAD
-            .from('books')
->>>>>>> bfbc6da (refactor: alterado nome da class)
-=======
-            .from('Ebooks')
->>>>>>> 06797b4 (refactor: alterada a query)
             .select('*')
-            .order('ebo_title', ascending: true);
+            .order('title', ascending: true);
 
         final List data = response;
         return data.map((item) => ResultSearchDto.fromJson(item)).toList();
       }
 
       // Caso haja texto de busca, filtra por título OU autor
-<<<<<<< HEAD
       final response = await supabase
           .from('vw_ebook')
-=======
-      final response = await supabaseClient
-<<<<<<< HEAD
-          .from('books')
->>>>>>> bfbc6da (refactor: alterado nome da class)
-=======
-          .from('Ebooks')
->>>>>>> 06797b4 (refactor: alterada a query)
           .select('*')
-          .or('ebo_title.ilike.%$searchText%,ebo_author.ilike.%$searchText%')
-          .order('ebo_title', ascending: true);
+          .or('title.ilike.%$searchText%,autor.ilike.%$searchText%')
+          .order('title', ascending: true);
 
       final List data = response;
       return data.map((item) => ResultSearchDto.fromJson(item)).toList();
