7cf7437a999f210b5d068008b9184dd3937eee8e
adriana.santos.sudoeste <adriana.santos.sudoeste@gmail.com>
2025-11-01 14:58:57 +0000

    feat: removido message

--- lib/utils/exceptions/app_exception.dart
+++ lib/utils/exceptions/app_exception.dart
@@ -1,6 +1,4 @@
-
 abstract class AppException implements Exception {
-  final String message;
   final StackTrace? stackTrace;
-  const AppException({required this.message, this.stackTrace});
+  const AppException({this.stackTrace});
 }
