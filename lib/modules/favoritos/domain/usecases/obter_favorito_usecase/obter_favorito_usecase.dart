e70158bc21dce1d012384c188e6bbda3447935bd
adrianaBispo283 <adriana.bispo283@gmail.com>
2025-11-03 21:36:12 -0300

    refactor: renomeado para ObterFavoritoUsecase

--- lib/modules/favoritos/domain/usecases/obter_favorito_usecase/obter_favorito_usecase.dart
+++ lib/modules/favoritos/domain/usecases/obter_favorito_usecase/obter_favorito_usecase.dart
@@ -1,6 +1,6 @@
 import 'package:myapp/modules/modules.dart';
 import 'package:myapp/utils/utils.dart';
 
-abstract class AddFavoriteUsecase {
+abstract class ObterFavoritoUsecase {
   Future<Either<AppException, void>> call(int idUser);
 }
