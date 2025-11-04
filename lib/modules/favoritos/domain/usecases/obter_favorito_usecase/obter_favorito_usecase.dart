6262e4f560ac97db879f97f3d4c9944ac98c71fa
adrianaBispo283 <adriana.bispo283@gmail.com>
2025-11-03 21:19:38 -0300

    feat: criado obter_favorito_usecase

--- lib/modules/favoritos/domain/usecases/obter_favorito_usecase/obter_favorito_usecase.dart
+++ lib/modules/favoritos/domain/usecases/obter_favorito_usecase/obter_favorito_usecase.dart
@@ -2,5 +2,5 @@ import 'package:myapp/modules/modules.dart';
 import 'package:myapp/utils/utils.dart';
 
 abstract class AddFavoriteUsecase {
-  Future<Either<AppException, void>> call(FavoritoEntity favorite, int idUser);
+  Future<Either<AppException, void>> call(int idUser);
 }
