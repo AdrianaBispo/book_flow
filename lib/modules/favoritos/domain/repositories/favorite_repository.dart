9686b44ad14bd881100f395cfa625f56cfa556b8
adrianaBispo283 <adriana.bispo283@gmail.com>
2025-11-03 21:33:03 -0300

    feat: criado FavoritesRepository

---
+++ lib/modules/favoritos/domain/repositories/favorite_repository.dart
@@ -0,0 +1,7 @@
+import 'package:myapp/modules/modules.dart';
+import 'package:myapp/utils/utils.dart';
+
+abstract class FavoritesRepository {
+  Future<Either<AppException, List<FavoritoEntity>>> getFavorites(int favoriteId);
+  Future<Either<AppException, bool>> removeFavorite(FavoritoEntity favorite);
+}
