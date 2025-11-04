2e8f58d79da7717770ec7d6e488f5d2fec2a05de
adrianaBispo283 <adriana.bispo283@gmail.com>
2025-11-03 21:20:03 -0300

    refactor: alterado o parametro de call

--- lib/modules/favoritos/domain/usecases/remover_favorito_usecase/remover_favorito_usecase.dart
+++ lib/modules/favoritos/domain/usecases/remover_favorito_usecase/remover_favorito_usecase.dart
@@ -2,5 +2,5 @@ import 'package:myapp/modules/modules.dart';
 import 'package:myapp/utils/utils.dart';
 
 abstract class RemoverFavoritoUsecase {
-  Future<Either<AppException, void>> call(String favoriteId);
+  Future<Either<AppException, void>> call(int favoriteId);
 }
