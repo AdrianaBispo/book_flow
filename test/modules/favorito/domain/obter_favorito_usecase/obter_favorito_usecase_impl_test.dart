9656f89556d34420083892568f5541b614850e1f
adrianaBispo283 <adriana.bispo283@gmail.com>
2025-11-04 21:48:28 -0300

    test: criado teste de ObterFavoritoUsecaseImpl

---
+++ test/modules/favorito/domain/obter_favorito_usecase/obter_favorito_usecase_impl_test.dart
@@ -0,0 +1,41 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:myapp/modules/favoritos/domain/domain.dart';
+import 'package:myapp/utils/utils.dart';
+
+class MockFavoritoRepository extends Mock implements FavoritesRepository {}
+
+class FakeAppException extends AppException {
+  FakeAppException() : super();
+}
+
+void main() {
+  late MockFavoritoRepository favoritoRepository;
+  late ObterFavoritoUsecaseImpl usecase;
+
+  setUp(() {
+    favoritoRepository = MockFavoritoRepository();
+    usecase = ObterFavoritoUsecaseImpl(repository: favoritoRepository);
+  });
+  
+  test('deve retornar List<FavoritoEntity> quando for  bem-sucedida', () async {
+    when(
+      () => favoritoRepository.obterFavoritos(any()),
+    ).thenAnswer((_) async => const Right(<FavoritoEntity>[]));
+
+    final result = await usecase.call(1);
+    expect(result.isRight(), true);
+    expect(result.getOrElse(() => []), isA<List<FavoritoEntity>>());
+  });
+
+  test('deve retornar AppException em caso de erro', () async {
+    when(
+      () => favoritoRepository.obterFavoritos(any()),
+    ).thenAnswer((_) async => Left(FakeAppException()));
+
+    final result = await usecase.call(1);
+
+    expect(result.isLeft(), true);
+  });
+}
