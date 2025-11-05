83558e9773362a0149d870c6de842aec0a51bc18
adrianaBispo283 <adriana.bispo283@gmail.com>
2025-11-04 21:32:44 -0300

    fix: renomeado e alterado o nome do parametro

--- lib/modules/favoritos/domain/repositories/favorite_repository.dart
+++ lib/modules/favoritos/domain/repositories/favorite_repository.dart
@@ -2,6 +2,6 @@ import 'package:myapp/modules/modules.dart';
 import 'package:myapp/utils/utils.dart';
 
 abstract class FavoritesRepository {
-  Future<Either<AppException, List<FavoritoEntity>>> getFavorites(int favoriteId);
+  Future<Either<AppException, List<FavoritoEntity>>> obterFavoritos(int idUser);
   Future<Either<AppException, void>> removerFavorito(int favoriteId);
 }
