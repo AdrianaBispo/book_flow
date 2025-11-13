1bb3c34aaf2b51022a0fdca7d4ac52c4879dcbba
adrianaBispo283 <adriana.bispo283@gmail.com>
2025-11-13 20:48:46 -0300

    refactor: atualizado o nome da entidade

--- lib/modules/favoritos/domain/repositories/favorite_repository.dart
+++ lib/modules/favoritos/domain/repositories/favorite_repository.dart
@@ -2,6 +2,6 @@ import 'package:myapp/modules/modules.dart';
 import 'package:myapp/utils/utils.dart';
 
 abstract class FavoriteRepository {
-  Future<Either<AppException, List<FavoritoEntity>>> getFavorites(int idUser);
+  Future<Either<AppException, List<FavoritEntity>>> getFavorites(int idUser);
   Future<Either<AppException, void>> removeFavorite(int favoriteId);
 }
