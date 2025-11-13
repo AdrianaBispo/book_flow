42c986466e6cc2a18829e304c8011faffad2591a
adrianaBispo283 <adriana.bispo283@gmail.com>
2025-11-13 20:35:05 -0300

    refactor: renomeada funcções e metodos

--- lib/modules/favoritos/domain/repositories/favorite_repository.dart
+++ lib/modules/favoritos/domain/repositories/favorite_repository.dart
@@ -1,7 +1,7 @@
 import 'package:myapp/modules/modules.dart';
 import 'package:myapp/utils/utils.dart';
 
-abstract class FavoritesRepository {
-  Future<Either<AppException, List<FavoritoEntity>>> obterFavoritos(int idUser);
-  Future<Either<AppException, void>> removerFavorito(int favoriteId);
+abstract class FavoriteRepository {
+  Future<Either<AppException, List<FavoritoEntity>>> getFavorites(int idUser);
+  Future<Either<AppException, void>> removeFavorite(int favoriteId);
 }
