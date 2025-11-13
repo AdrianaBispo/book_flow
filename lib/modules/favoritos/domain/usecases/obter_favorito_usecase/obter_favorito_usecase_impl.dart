7979b87a7b950ff713ff88b2e24c5071a1c187f5
adrianaBispo283 <adriana.bispo283@gmail.com>
2025-11-13 20:35:36 -0300

    refactore: atualizado o nome dos metodos

--- lib/modules/favoritos/domain/usecases/obter_favorito_usecase/obter_favorito_usecase_impl.dart
+++ lib/modules/favoritos/domain/usecases/obter_favorito_usecase/obter_favorito_usecase_impl.dart
@@ -3,12 +3,12 @@ import 'package:myapp/utils/utils.dart';
 import 'obter_favorito_usecase.dart';
 
 class ObterFavoritoUsecaseImpl implements ObterFavoritoUsecase {
-  final FavoritesRepository repository;
+  final FavoriteRepository repository;
 
   ObterFavoritoUsecaseImpl({required this.repository});
 
   @override
   Future<Either<AppException, List<FavoritoEntity>>> call(int idUser) async{
-    return await repository.obterFavoritos(idUser);
+    return await repository.getFavorites(idUser);
   }
 }
