3cbc8268408932a93919a2a7f9d58f8d2d632fd3
adrianaBispo283 <adriana.bispo283@gmail.com>
2025-11-13 20:49:56 -0300

    refactor: atualizado o nome da entidade

--- lib/modules/favoritos/domain/usecases/obter_favorito_usecase/obter_favorito_usecase_impl.dart
+++ lib/modules/favoritos/domain/usecases/obter_favorito_usecase/obter_favorito_usecase_impl.dart
@@ -8,7 +8,7 @@ class ObterFavoritoUsecaseImpl implements ObterFavoritoUsecase {
   ObterFavoritoUsecaseImpl({required this.repository});
 
   @override
-  Future<Either<AppException, List<FavoritoEntity>>> call(int idUser) async{
+  Future<Either<AppException, List<FavoritEntity>>> call(int idUser) async{
     return await repository.getFavorites(idUser);
   }
 }
