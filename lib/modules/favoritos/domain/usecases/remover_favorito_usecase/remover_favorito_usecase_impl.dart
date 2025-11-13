c1503b95bfed2e597d0466ef0d9a068b34339333
adrianaBispo283 <adriana.bispo283@gmail.com>
2025-11-13 20:40:18 -0300

    refactore: atualizado renomeado para removeFavorite

--- lib/modules/favoritos/domain/usecases/remover_favorito_usecase/remover_favorito_usecase_impl.dart
+++ lib/modules/favoritos/domain/usecases/remover_favorito_usecase/remover_favorito_usecase_impl.dart
@@ -3,12 +3,12 @@ import 'package:myapp/utils/utils.dart';
 import 'remover_favorito_usecase.dart';
 
 class RemoverFavoritoUsecaseImpl implements RemoverFavoritoUsecase {
-  final FavoritesRepository repository;
+  final FavoriteRepository repository;
 
   RemoverFavoritoUsecaseImpl({required this.repository});
 
   @override
   Future<Either<AppException, void>> call(int favoriteId) async{
-    return await repository.removerFavorito(favoriteId);
+    return await repository.removeFavorite(favoriteId);
   }
 }
