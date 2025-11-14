73bdebffe03c5a8780763338665bca737faf2b87
adrianaBispo283 <adriana.bispo283@gmail.com>
2025-11-13 21:32:02 -0300

    feat: criado FavoriteDatasourceRemoteImpl

---
+++ lib/modules/favoritos/data/datasources/remote/favorite_datasource_remote_impl.dart
@@ -0,0 +1,42 @@
+import 'package:myapp/modules/favoritos/data/dtos/dtos.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'favorite_datasource_remote.dart';
+
+class FavoriteDatasourceRemoteImpl implements FavoriteDatasourceRemote {
+  final SupabaseClient client;
+
+  FavoriteDatasourceRemoteImpl({required this.client});
+
+  @override
+  Future<List<FavoritoDto>> getFavoriteList({required int idUser}) async {
+    try {
+      final response = await client
+          .from('favorites')
+          .select()
+          .eq('user_id', idUser);
+
+      List<FavoritoDto> data = response
+          .map((toElement) => FavoritoDto.fromJson(toElement))
+          .toList();
+      return data;
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  @override
+  Future<void> removeFavorite({
+    required int favoriteId,
+    required int idUser,
+  }) async {
+    try {
+      await client
+          .from('favorites')
+          .delete()
+          .eq('id', favoriteId)
+          .eq('user_id', idUser);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+}
