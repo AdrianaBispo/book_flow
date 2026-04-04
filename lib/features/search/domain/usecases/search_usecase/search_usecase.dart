0db3c2dd82cf773b6573dd30e66e39cf36c205ee
adrianaBispo283 <adriana.bispo283@gmail.com>
2026-04-03 21:11:06 -0300

    style: arquivos não mais utilizados

--- lib/features/search/domain/usecases/search_usecase/search_usecase.dart
+++
@@ -1,6 +0,0 @@
-import 'package:myapp/modules/modules.dart';
-import 'package:myapp/core/utils/utils.dart';
-
-abstract class SearchUsecase {
-    Future<Either<AppException,List<ResultSearchEntity>>> call (String searchText);
-}
