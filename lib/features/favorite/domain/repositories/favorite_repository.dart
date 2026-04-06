075b953f36541e2a0aff125575379f43304ec6ea
adriana <adriana.bispo283@gmail.com>
2026-04-06 11:40:35 -0300

    TODO: ADICIONAR FAVORITO

--- lib/features/favorite/domain/repositories/favorite_repository.dart
+++ lib/features/favorite/domain/repositories/favorite_repository.dart
@@ -4,4 +4,5 @@ import 'package:myapp/core/utils/utils.dart';
 abstract class FavoriteRepository {
   Future<Either<AppException, List<FavoritEntity>>> getFavorites(int idUser);
   Future<Either<AppException, void>> removeFavorite(int favoriteId);
+  //TODO: ADICIONAR FAVORITO
 }
