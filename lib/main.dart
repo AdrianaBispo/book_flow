2cac01a5bcd580f8ddb3d86c6ddcb83e175c3761
adriana <adriana.bispo283@gmail.com>
2026-04-06 16:40:18 -0300

    feat: inicializado o hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
-import '../appwidget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import '../appwidget.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
 
+  // Initializing Hive
+  await Hive.initFlutter();
+  await Hive.openBox('library_box');
+
   await Supabase.initialize(
     url: dotenv.env['SUPABASE_URL']!,
     anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
   );
+  
   runApp(const AppWidget());
 }
