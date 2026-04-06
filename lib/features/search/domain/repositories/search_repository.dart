200a3b904265d94b8da941521884b8a79c9ad5ed
adriana <adriana.bispo283@gmail.com>
2026-04-06 11:36:07 -0300

    removed: removido

--- lib/features/search/domain/repositories/search_repository.dart
+++ lib/features/search/domain/repositories/search_repository.dart
@@ -5,7 +5,4 @@ abstract class SearchRepository {
   Future<Either<AppException,List<ResultSearchEntity>>> search(
     String searchText
   );
-  Future<Either<AppException, void>> addFavorite(int favoriteId);
-  Future<Either<AppException, void>> removeFavorite(int favoriteId);
-  Future<Either<AppException, void>> addInLibrary(int bookId);
 }
