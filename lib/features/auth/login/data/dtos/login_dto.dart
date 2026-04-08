ec724f2b9afbe244ae5645f698c56e4b28cfb2a4
adrianaBispo283 <adriana.bispo283@gmail.com>
2026-04-07 23:34:50 -0300

    refactor: ajustado dto

--- lib/features/auth/login/data/dtos/login_dto.dart
+++ lib/features/auth/login/data/dtos/login_dto.dart
@@ -1,16 +1,10 @@
 import '../../domain/domain.dart';
 
 class LoginDto extends UserLoginEntity {
-  String? email;
-  String? password;
-
-  LoginDto({required this.email, required this.password})
-    : super(email: email, password: password);
+  LoginDto({required super.email, required super.password});
 
   LoginDto.fromJson(Map<String, dynamic> map)
-    : email = map['email'],
-      password = map['password'],
-      super(email: map['email'], password: map['password']);
+    : super(email: map['email'], password: map['password']);
 
   Map<String, dynamic> toJson() {
     return {'email': email, 'password': password};
